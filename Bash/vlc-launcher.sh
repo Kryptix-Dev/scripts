7612ad0e44be9598f52ba8b813876b67b232789f
Kryptix <64878608+Kryptix-Dev@users.noreply.github.com>
2022-07-02 16:52:33 -0500

    Updated to 1.1
    Ver 1.1
    	Added pactl command to set volume
    	Added sleep to allow delay before volume changes
    	Run 'pactl list sinks' to get device ID for active audio device

--- Bash/vlc-launcher.sh
+++ Bash/vlc-launcher.sh
@@ -1,13 +1,17 @@
 #=====================================================================================#
-#Ver 1.0
-#	Created launcher to play music library without launching and manually starting
-#	VLC. It will also play next track at random and suppress errors.
-#	Verified script with ShellCheck [https://www.shellcheck.net/]
+#Ver 1.1
+#	Added pactl command to set volume
+#	Added sleep to allow delay before volume changes
+#	Run 'pactl list sinks' to get device ID for active audio device
 #=====================================================================================#
 #!/bin/bash
 
 if pgrep 'vlc' ; then
 	pkill vlc
+	sleep 0.25s
+	pactl set-sink-volume 277 50%
 else 
+	pactl set-sink-volume 277 25%
+	sleep 0.25s
 	cvlc --random --quiet $HOME/Music/
 fi
