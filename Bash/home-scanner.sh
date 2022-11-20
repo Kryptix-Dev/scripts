8c578de8e1fb0bf21eedd6e694ef6f09e9ccbf9c
Kryptix <64878608+Kryptix-Dev@users.noreply.github.com>
2022-11-20 17:59:44 -0600

    Updated to 1.1
    Added freshclam log file to check for update errors
    Added a day of the week checker to determine if a full scan is run
    Added a short scan option when it isn't Saturday
    Added to the if-else notification loop to notify on database update errors
    Updated the failed notification message
    Verified with ShellCheck

--- Bash/home-scanner.sh
+++ Bash/home-scanner.sh
@@ -1,26 +1,60 @@
+#=====================================================================================#
+#Ver 1.1
+#	Added freshclam log file to check for update errors
+#	Added a day of the week checker to determine if a full scan is run
+#	Added a short scan option when it isn't Saturday
+#	Added to the if-else notification loop to notify on database update errors
+#	Updated the failed notification message
+#	Verified with ShellCheck
+#=====================================================================================#
 #!/bin/bash
-# Change path $HOME/System/... to point to where the script will reside
 
-rm "$HOME/System/clamav-daily.log"
-freshclam
-	#notify-send "Virus database updated, commencing scan." --icon=selection-mode
-nice --adjustment=5 clamscan "$HOME/" -ri --exclude-dir="$HOME/.local/share/Trash" --scan-archive --log="$HOME/System/clamav-daily.log"
+rm "$HOME/System/Logs/clamav.log"
+rm "$HOME/System/Logs/freshclam.log"
 
-dayOfMonth=$(date '+%d')
-monthOfYear=$(date '+%b')
-if [ "$dayOfMonth" == 01 ]; then
-		mv "$HOME/System/clamav-month.log" "$HOME/System/clamav-${monthOfYear,,}.log"
-#		rm "$HOME/System/clamav-month.log"
-		cat "$HOME/System/clamav-daily.log" >> "$HOME/System/clamav-month.log"
+freshclam --quiet --log="$HOME/System/Logs/freshclam.log"
+
+#=====================================================================================#
+# CHECK FOR THE DAY OF WEEK TO DETERMINE IF FULL SCAN IS RUN
+# Run full scan on Saturdays with a nice code so it's deprioritized
+# Run a Download folder scan for rest of the week
+#=====================================================================================#
+dayOfWeek=$(date '+%a')
+	if [ "$dayOfWeek" = "Sat" ]; then
+		nice --adjustment=5 clamscan "$HOME/" -ri --exclude-dir="$HOME/.local/share/Trash" --scan-archive --log="$HOME/System/Logs/clamav.log"
 	else
-		cat "$HOME/System/clamav-daily.log" >> "$HOME/System/clamav-month.log"
+		clamscan "$HOME/Downloads" -ri --scan-archive --log="$HOME/System/Logs/clamav.log"
+	fi
+
+#=====================================================================================#
+# IF-ELSE TO CHECK LOG FILES FOR NOTIFICATION
+#=====================================================================================#
+updateFailed="$(grep -Po 'ERROR: Update \K.*' "$HOME/System/Logs/freshclam.log")"
+infectedFiles="$(grep -Po 'Infected files: \K.*' "$HOME/System/Logs/clamav.log")"
+if [ "$updateFailed" != "failed." ] && [ "$infectedFiles" -eq 0 ]; then
+	notify-send "Scan is GREEN" "No threats found, database updated. $updateFailed" --icon=selection-mode
+elif [ "$updateFailed" == "failed." ] && [ "$infectedFiles" -eq 0 ]; then
+	notify-send "Scan is YELLOW" "No threats found, but database failed to update." --icon=mail-mark-junk
+elif [ "$infectedFiles" -gt 0 ]; then
+	notify-send "Scan is RED" "Scan found <b>$infectedFiles</b> infected files, check log for details." --icon=mail-mark-junk
+elif [ "$updateFailed" == "failed." ] && [ "$infectedFiles" -gt 0 ]; then
+	notify-send "Scan is RED" "Scan found <b>$infectedFiles</b> infected files, check log for details.  Database failed to update." --icon=mail-mark-junk
+else 
+	notify-send "Scan FAILED" "The scan was cancelled or failed." --icon=process-stop
 fi
 
-infectedFiles="$(grep -Po 'Infected files: \K.*' "$HOME/System/clamav-daily.log")"
-if [ "$infectedFiles" -eq 0 ]; then
-		notify-send "Scan is GREEN" "Scan results are clean, no threats found." --icon=selection-mode
-	elif [ "$infectedFiles" -gt 0 ]; then
-		notify-send "Scan is RED" "Scan found <b>$infectedFiles</b> infected files, check the log for details." --icon=mail-mark-junk
-	else 
-		notify-send "Scan FAILED" "Something went wrong, the scan will need to run again." --icon=process-stop
-fi
+#=====================================================================================#
+# LOG FILE ARCHIVAL
+# Combine daily log files into a monthly log file on the first day of the month
+# Combine monthly log files into a yearly log file on the first day of the month
+#
+#dayOfMonth=$(date '+%d')
+#monthOfYear=$(date '+%b')
+#if [ "$dayOfMonth" == 01 ]; then
+#		mv "$HOME/System/Logs/clamav-month.log" "$HOME/System/Logs/clamav-${monthOfYear,,}.log"
+#		rm "$HOME/System/Logs/clamav-month.log"
+#		cat "$HOME/System/Logs/clamav-daily.log" >> "$HOME/System/Logs/clamav-month.log"
+#	else
+#		cat "$HOME/System/Logs/clamav-daily.log" >> "$HOME/System/Logs/clamav-month.log"
+#fi
+#=====================================================================================#
